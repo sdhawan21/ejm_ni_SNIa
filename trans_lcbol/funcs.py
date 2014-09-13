482fe672cbcce24571aa257ff323cd24ed5d7b56
sdhawan21 <suhail.dhawan@gmail.com>
2014-09-13 14:35:00 +0200

    Functions in python for bolometric lc (translate Stephane's code)

---
+++ trans_lcbol/funcs.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sys 
+
+pt='/Users/lapguest/all_paper/files_snpy/'
+tmax=np.loadtxt(pt+'tmax_dm15.dat', dtype='string')
+bselect=sys.argv[1]
+class flux_conv:   #conversion of light curves to filter fluxes, no propagation of distances yet. (do so after the trapezoidal) 
+	def spt_conv(self, mag, zp):
+                return pow(10, -0.4*(mag-zp)) 
+        def all_time(self, pharr):
+                arr=[]
+                for i in pharr:
+                        fl=self.spt_conv(i[1], zp)
+                        arr.append(fl)
+                return np.array(arr)
+
+                        
+
+class red_corr:
+	
+
+
