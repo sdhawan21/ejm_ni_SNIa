a9152f36cf80a8c86e8b4880a933b2c874fd7b1d
sdhawan21 <suhail.dhawan@gmail.com>
2014-09-16 15:59:04 +0100

    New version, tries to the trapezoidal valenti style, a little work to be done

--- trans_lcbol/funcs.py
+++ trans_lcbol/funcs.py
@@ -4,24 +4,34 @@ import sys
 pt='/Users/lapguest/all_paper/files_snpy/'
 tmax=np.loadtxt(pt+'tmax_dm15.dat', dtype='string')
 bselect=sys.argv[1]
+pbin=np.loadtxt("csp_info.dat", dtype='string')
 class flux_conv:   #conversion of light curves to filter fluxes, no propagation of distances yet. (do so after the trapezoidal) 
 	def spt_conv(self, mag, zp):
                 return pow(10, -0.4*(mag-zp)) 
-        def all_time(self, pharr):
+        def all_time(self, pharr, filt):
                 arr=[]
+                rr=np.array([i[0] for i in pbin[:,0]])
+                zp=float(pbin[rr==filt][0][3])
                 for i in pharr:
                         fl=self.spt_conv(i[1], zp)
                         arr.append(fl)
                 return np.array(arr)
 
                 
-
+class dist:
+        def mu2cm(self, mu):
+                mpc2=3.08e24**2
+                ff=4*np.pi*pow(10, ((mu-25)/5.0))
+                return ff*mpc2
 class interp:
 #Valenti style integration (trapezoidal)
-	def fl_int(self,flarr):
-           for i in range(nfilt):
-                   dist=lamb[i+1]-lamb[i]
+	def fl_int(self,m):
+           fl_int=0
+           lamb=pbin[:,1].astype('float32')
+           for i in range(nfilt-1):
+                   filt=flux_conv().all_time(f, )
+                   dist=lamb[rr==m1][0]-lamb[rr==m][0]
                    fluxmean=0.5*(flarr)
-                   fl_int+=fluxmean*dist
-
+           #fl_int+=fluxmean*dist
+           return fluxmean*dist
 
--- trans_lcbol/main.py
+++ trans_lcbol/main.py
@@ -1,5 +1,16 @@
 import funcs as fn
+import mag2fl as mf
+import numpy as np
+barr=[i[0] for i in fn.pbin[:,0]]
+flint=0
+for m in barr:
+    try:
+        lc=mf.conv().rd_lc('SN2007on', m)
+        f=np.zeros([len(lc['MJD']), 2]);f[:,0]=lc['MJD'];f[:,1]=lc[m]
+        arfun=fn.flux_conv().all_time(f, m)
+        flint+=fl_int(m)
+        #print fn.interp().fl_int(arfun, 4)*fn.dist().mu2cm(31.45)
+    except:
+        m
 
-
-
-
+#
