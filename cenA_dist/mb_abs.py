0b097c808b8be001a7a9c289daece42eea3d0460
sdhawan21 <suhail.dhawan@gmail.com>
2014-10-01 14:23:57 +0200

    New mb calculation code

--- cenA_dist/mb_abs.py
+++ cenA_dist/mb_abs.py
@@ -5,7 +5,7 @@ import sys
 from scipy.interpolate import interp1d
 
 
-
+rn=np.random.normal
 def interp_mb():
 	s1=np.loadtxt('../ni_logbol_c00.txt', usecols=(1, 3))
 	s1[:,1]=pow(10, s1[:,1])
@@ -19,15 +19,19 @@ def interp_mb():
 	l=np.linspace(lbol.min(), lbol.max())
 	spl=gl(l)
 	tl=float(sys.argv[1])
+	etl=float(sys.argv[2])
+	ex_arr=[]
 	pf=np.polyfit(lbol, mb, 2)
 	bf=pf[0]*tl**2+pf[1]*tl+pf[2]
-	print bf
-	plt.plot(lbol, mb, 'r+')
-	plt.show()
-	l1=abs(l-tl)
-	print spl[l1==min(l1)]
-	
-if len(sys.argv)==2:	
-	interp_mb()
+	#print bf
+	#plt.plot(lbol, mb, 'r+')
+	#plt.show()
+	for k in range(1000):
+		l1=abs(l-rn(tl, etl))
+		ex=spl[l1==min(l1)]
+		ex_arr.append(ex[0])
+	return np.mean(ex_arr), np.std(ex_arr)
+if len(sys.argv)==3:	
+	print interp_mb()[0]+14.111-30.91, interp_mb()[1]+0.14+0.011
 else:
-	
+	print "Usage: python "+sys.argv[0]+"<Lbol peak > <e_Lbol>"
