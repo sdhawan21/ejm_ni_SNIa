891cfefe178402cf9de071fdb91e9ffd48664ed4
sdhawan21 <suhail.dhawan@gmail.com>
2014-10-09 18:12:02 +0200

    Documentation for the ejecta mass code

--- src/mej_eq.py
+++ src/mej_eq.py
@@ -8,48 +8,114 @@ from astropy.modeling import fitting, models
 from scipy.interpolate import interp1d
 from scipy.optimize import leastsq, curve_fit
 #from bol_lc import bol_cl, lpeak_ni
-ds2=86400.0			#number of seconds in a day
+
+
+#number of seconds in a day
+ds2=86400.0
+#e-fold times for nickel and cobalt (56, resp.)			
 lni=1/(8.8)
 lco=1/(111.3)		
-fac=624150.647996		#Mev to ergs
+
+#Mev to ergs
+fac=624150.647996		
+
+#input SN from command line
 obj=sys.argv[1]
+
+#some sort of scale factor
 sc=1e10
+
+#array of times 
 tarr=np.linspace(50, 100, 100)
+
+
 class mej:
+	"""
+	defined the class for the required functions
+	"""
+	
 	def edp(self,  t, Nni, t0):
-		t1=lni*Nni*np.exp(-lni*t)*(1.75)		#ENi
-		t2=lco*Nni*(lni/(lni-lco))*(np.exp(-lco*t)-np.exp(-lni*t))	#ECo_e+
-		t3=(0.12)+(3.61)*(1-np.exp(-(t0/t)**2))		#ECo_gamma
+		"""
+		function for energy deposition; equation in Nadyozhin 1994
+		"""
+		#ENi
+		t1=lni*Nni*np.exp(-lni*t)*(1.75)		
+		#ECo_e+
+		t2=lco*Nni*(lni/(lni-lco))*(np.exp(-lco*t)-np.exp(-lni*t))	
+		#ECo_gamma
+		t3=(0.12)+(3.61)*(1-np.exp(-(t0/t)**2))		
 		return (t1+t2*t3)/(fac*ds2)#(t2*t3)/(fac*ds2)
+	
+	
 	def rd_lc(self, sn, bands):
+		"""
+		read in bolometric lc
+		"""
 		return np.loadtxt('lcbol_distrib/'+sn+'_lcbol_'+bands+'.dat')
+		
 	def numb(self, mni):
+		"""
+		converts the nickel mass to number of nickel nuclei (precision checked, no overflow errors here)
+		"""
 		return mni*1.98e33*6.022e23/56.0
+		
 	def p2ni(self, sn, bands):
+		"""
+		function to calculate the Ni mass when input isnt given. Cubic spline interpolation to the peak
+		"""
 		t=self.rd_lc(sn, bands)
 		r1=np.random.normal(t[:,1], t[:,2])
 		p=interp1d(t[:,0], r1, kind='cubic')
 		o=np.linspace(min(t[:,0]), max(t[:,0]), 100)
 		u=p(o)
 		return o[u==max(u)], max(u) 
+		
 	def ran(self, arr1, arr2):
+		"""
+		generic function to define the range for selection in an array
+		"""
+
 		return arr1[(arr1>=40) & (arr1<=100)], arr2[(arr1>=40) & (arr1<=100)]
 	def slp(self, arr1, arr2):
+		"""
+		least squares fit for late decline rate (no errors)
+		
+		TODO: errors, ODR/statsmodels implementations
+		"""
 		a=np.vstack([arr1, np.ones(len(arr1))]).T
 		return np.linalg.lstsq(a, arr2)[0]
+		
+#input nickel mass (nim)
 nim=float(sys.argv[2])#mej().p2ni(obj,'BVRIJH')[1]/2e43
 #print nim
+
+#define the set of bands using which the bolometric light curve has been calculated 
+#note: for our sample UBVRIJH since we've selected as such
 bands=sys.argv[3]
+
+#input distance
 dm=float(sys.argv[4])
+
+#rise time calculation from the Dm15 input
 rt=17.5-5*(dm-1.1)
+
+#read in the bolometric light curve 
 lcbol=mej().rd_lc(obj, bands)
+
+#shift time axis wrt bolometric peak
 ph=lcbol[:,0]-mej().p2ni(obj,bands)[0]
+
 #print ph
 #lcbol[:,1]*=8.6e25**2
+
 l1=np.linspace(0, 50, 100)
 l2=np.linspace(-20, 100, 100)
+
+#late decline region of LC
 ph1,mag1=mej().ran(ph, lcbol[:,1])
+#slope of log flux (2.5 factor not applied)
 m=mej().slp(ph1, np.log10(mag1))
+
 ed=m[0]*ph1+m[1]
 func=mej().edp
 #print leastsq(func, mag1, args=(mej().numb(nim)))
